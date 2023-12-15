9492f2ac473471b6ed13b5860fe24fcfa86c465a
julianmpineda <82466536+julianmpineda@users.noreply.github.com>
2023-12-15 16:52:14 -0500

    Update day02.java
    Realized while doing Part 4 that the first "i" for loop doesn't do anything besides take it from the original list and make 1 split and put it in a new list that I then pull from and cut again in the next "j" for loop, making it redundant, so I've combined them.

--- day02.java
+++ day02.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class day2 {
+public class day02 {
     public static void main(String[] args) {
 
         List<String> bag = new ArrayList<String>();
@@ -16,31 +16,26 @@ public class day2 {
             e.printStackTrace();
         }
 
-        List<String[]> games = new ArrayList<String[]>();
+
         int sum = 0, powSum = 0;
 
         //Creates list of array of games from bag
         for (int i = 0; i < bag.size(); i++) {
-            //very ugly
-            games.add(bag.get(i).substring(bag.get(i).indexOf(':') + 2).split(";"));
-        }
 
-        // Parse each game
-        // Get game
-        for (int j = 0; j < bag.size(); j++) {
+            // very ugly
+            String[] game = bag.get(i).substring(bag.get(i).indexOf(':') + 2).split(";");
+
             int redMax = 0, blueMax = 0, greenMax = 0;
-            String[] temp = games.get(j);
             Boolean possible = true;
 
             //break game into rounds
-            for (int k = 0; k < temp.length; k++) {
-                String[] cubes = temp[k].split("[,]");
+            for (int j = 0; j < game.length; j++) {
+                String[] cubes = game[j].split("[,]");
 
                 //break rounds into pulls
                 for (int l = 0; l < cubes.length; l++) {
                     String[] pull = cubes[l].trim().split(" ");
                     int num = Integer.parseInt(pull[0]);
-                  
                     if (pull[1].equals("red")) {
                         redMax = Math.max(num, redMax);
                         if (num > 12) {
@@ -63,7 +58,7 @@ public class day2 {
 
             powSum += (redMax * blueMax * greenMax);
             if (possible) {
-                sum += j + 1;
+                sum += i + 1;
             }
         }
 
@@ -71,4 +66,3 @@ public class day2 {
         System.out.println("Part 2: " + powSum);
     }
 }
-
